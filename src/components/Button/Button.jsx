480637e5e569aaa9d8840eeda0eb32897be3ee84
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-06-23 12:03:39 +0300

    button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,9 +3,11 @@ import styles from './Button.module.css';
 
 const Button = (props) => {
     return (
-        <button {...props} className={styles.button + props.className}/>
+        <button {...props} className={styles.button}/>
     )
 }
+
+// + props.className
 
 export default Button;
 
